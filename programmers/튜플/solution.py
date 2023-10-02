c30fefd0c249d2363fa2a61b2952f17b2ca59290
Dayeon Ku <79597984+9ooDa@users.noreply.github.com>
2023-10-02 23:01:27 +0900

    updated at 10/2/2023, 11:01:26 PM

--- programmers/튜플/solution.py
+++ programmers/튜플/solution.py
@@ -3,6 +3,7 @@
 def solution(s):
     s_list = list(map(lambda x: [int(i) for i in x], [x.replace('{', '').replace('}', '').split(',') for x in s.split('},')]))
     
+    # an의 value는 바뀌지 않기 때문에 정렬해준 다음 길이가 짧은 리스트부터 고려하기
     elems = []
     for sl in sorted(s_list, key=lambda x: len(x)):
         for num in sl:
