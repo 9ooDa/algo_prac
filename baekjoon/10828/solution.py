f8b89f7e555d5dcd7f78eb511b2c7fb50a02aae8
Dayeon Ku <79597984+9ooDa@users.noreply.github.com>
2023-10-07 17:36:16 +0900

    uploaded at 10/7/2023, 5:36:16 PM

---
+++ baekjoon/10828/solution.py
@@ -0,0 +1,34 @@
+// [문제 링크]: https://www.acmicpc.net/problem/10828
+
+import sys
+​
+n = int(sys.stdin.readline().rstrip())
+​
+arr = [sys.stdin.readline().rstrip().split() for _ in range(n)]
+​
+stack = []
+​
+for l in arr:
+    if l[0] == 'push':
+        stack.append(int(l[1]))
+    
+    elif l[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+​
+    elif l[0] == 'size':
+        print(len(stack))
+​
+    elif l[0] == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(1)
+    
+    elif l[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
