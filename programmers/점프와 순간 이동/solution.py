58f0cf144d78db2c71c50b1b5217dc817c1cfa55
Dayeon Ku <79597984+9ooDa@users.noreply.github.com>
2023-10-08 18:03:28 +0900

    uploaded at 10/8/2023, 6:03:27 PM

---
+++ programmers/점프와 순간 이동/solution.py
@@ -0,0 +1,12 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12980
+
+def solution(n):
+    battery = 0
+    while n != 0:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n -= 1
+            battery += 1
+            
+    return battery
