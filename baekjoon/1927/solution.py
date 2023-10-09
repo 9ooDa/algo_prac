f4a05d9166138a0000ccdf3cedbc0352e977fb98
Dayeon Ku <79597984+9ooDa@users.noreply.github.com>
2023-10-09 16:24:07 +0900

    uploaded at 10/9/2023, 4:24:07 PM

---
+++ baekjoon/1927/solution.py
@@ -0,0 +1,17 @@
+// [문제 링크]: https://www.acmicpc.net/problem/1927
+
+import heapq
+import sys
+​
+n = int(sys.stdin.readline().rstrip())
+h = []
+for i in range(n):
+    num = int(sys.stdin.readline().rstrip())
+​
+    if num == 0:
+        if len(h) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(h))
+    else:
+        heapq.heappush(h, num)
