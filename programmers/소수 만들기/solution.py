a3b8a4186d09de45dff3be2b4bfe2f32f766398d
Dayeon Ku <79597984+9ooDa@users.noreply.github.com>
2023-10-09 21:46:55 +0900

    uploaded at 10/9/2023, 9:46:54 PM

---
+++ programmers/소수 만들기/solution.py
@@ -0,0 +1,36 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12977#
+
+from itertools import combinations
+
+def solution(nums):
+    def is_prime(x):
+        prime = True
+        for i in range(2, x):
+            if x % i == 0:
+                prime = False
+        return prime
+    
+#     ans = []
+#     res = []
+#     comb = []
+#     def back_track(start):
+#         if len(ans) == 3:
+#             comb.append(list(map(int, ans)))
+#             return
+        
+#         for i in range(start, len(nums)):
+#             if nums[i] not in ans:
+#                 ans.append(nums[i])
+#                 back_track(start + 1)
+#                 ans.pop()
+                
+#     back_track(0)
+    
+    comb = list(combinations(nums, 3))
+    count = 0
+    for c in comb:
+        num = sum(c)
+        if is_prime(num):
+            count += 1
+            
+    return count
