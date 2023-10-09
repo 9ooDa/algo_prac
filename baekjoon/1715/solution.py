8b29244fc40f5ff42ed3fc6a96bb582e1e5ab8dd
Dayeon Ku <79597984+9ooDa@users.noreply.github.com>
2023-10-09 17:33:13 +0900

    uploaded at 10/9/2023, 5:33:13 PM

---
+++ baekjoon/1715/solution.py
@@ -0,0 +1,21 @@
+// [문제 링크]: https://www.acmicpc.net/problem/1715
+
+import sys
+import heapq
+​
+n = int(sys.stdin.readline().rstrip())
+​
+res = []
+h = []
+for _ in range(n):
+    num = int(sys.stdin.readline().rstrip())
+​
+    heapq.heappush(h, num)
+​
+while len(h) > 1:
+    a = heapq.heappop(h)
+    b = heapq.heappop(h)
+    heapq.heappush(h, a + b)
+    heapq.heappush(res, a + b)
+    
+print(sum(res))
