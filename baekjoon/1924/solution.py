4274b660725ce308b8ce4821bdd717e757b72a48
구다연_T6009 <79597984+9ooDa@users.noreply.github.com>
2023-11-19 22:19:25 +0900

    uploaded at 11/19/2023, 10:19:21 PM

---
+++ baekjoon/1924/solution.py
@@ -0,0 +1,14 @@
+// [문제 링크]: https://www.acmicpc.net/problem/1924
+
+import sys
+​
+x, y = map(int, sys.stdin.readline().rstrip().split())
+​
+days_in_month = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+remaining_dict = {0:'SUN', 1:'MON', 2:'TUE', 3:'WED', 4:'THU', 5:'FRI', 6:'SAT'}
+days = y
+​
+for i in range(1, x):
+    days += days_in_month[i]
+​
+print(remaining_dict[days % 7])
