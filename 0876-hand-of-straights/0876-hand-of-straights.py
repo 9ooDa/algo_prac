e963dc37d5b16b27e2984841c89aea6c13e1572a
Dayeon Ku <79597984+9ooDa@users.noreply.github.com>
2024-06-07 00:30:29 +0900

    Time: 1321 ms (6.15%), Space: 18.1 MB (94.17%) - LeetHub

---
+++ 0876-hand-of-straights/0876-hand-of-straights.py
@@ -0,0 +1,27 @@
+import heapq
+
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+        
+        ans = True
+        temp = []
+        while hand:
+            if len(temp) == groupSize:
+                temp = []
+
+            hand.sort()
+
+            if not temp:
+                temp.append(hand.pop(0))
+            else:
+                if temp[-1] + 1 in hand:
+                    nxt = temp[-1] + 1
+                    temp.append(nxt)
+                    hand.remove(nxt)
+                else:
+                    ans = False
+                    break
+
+        return ans
